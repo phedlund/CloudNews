248ab1442ffb2829ebbe3914f141f9ee1f507eaf
Peter Hedlund <support@pbh.dev>
2021-06-13 13:37:01 -0700

    Fix crash when going to background

--- iOCNews/ItemsViewController.swift
+++ iOCNews/ItemsViewController.swift
@@ -93,11 +93,6 @@ class ItemsViewController: BaseCollectionViewController {
         observers.append(NotificationCenter.default.addObserver(forName: UIContentSizeCategory.didChangeNotification, object: nil, queue: .main, using: { [weak self] _ in
             self?.collectionView.reloadData()
         }))
-        observers.append(NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: .main, using: { [weak self] _ in
-            if let visibleItems = self?.collectionView.indexPathsForVisibleItems {
-                self?.collectionView.reloadItems(at: visibleItems)
-            }
-        }))
         if #available(iOS 14.0, *) {
             observers.append(NotificationCenter.default.addObserver(forName: .displayModeChanged, object: nil, queue: .main, using: { [weak self] notification in
                 if let userInfo = notification.userInfo, let displayMode = userInfo["NewMode"] as? UISplitViewController.DisplayMode {
